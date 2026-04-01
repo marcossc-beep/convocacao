e14f8f286c630bee7b1756a2060ec10a54843b0c
unknown <Professor@SN-392513.edu.fiesc.com.br>
2026-04-01 11:30:20 -0300

    adicionando gitignore

---
+++ banco.sql
@@ -0,0 +1,6 @@
+CREATE TABLE CONVOCACAO (
+    id serial,
+    nome VARCHAR(255),
+    posicao VARCHAR(255)
+)
+
--- server.js
+++ server.js
@@ -16,6 +16,34 @@ servidor.get('/convocados', async () => {
     return resultado.rows
 })
 
+servidor.post('/convocacao', async (request) => {
+    const body = request.body
+    const resultado = await sql.query(
+        'INSERT INTO convocacao (nome, posicao) VALUES ($1, $2)',
+        [body.nome, body.posicao]
+    );
+    return {mensagem: "Deu boa feio!"}
+})
+
+servidor.put('/convocacao/:id', async (request) => {
+    const body = request.body
+    const id = request.params.id
+    const resultado = await sql.query(
+        'UPDATE convocacao set nome = $1, posicao = $2 WHERE id = $3',
+        [body.nome, body.posicao, id]
+    )
+    return {mensagem: "Deu boa feio!"}
+})
+
+servidor.delete('/convocacao/:id', async (request) => {
+    const id = request.params.id;
+    const resultado = await sql.query(
+        'DELETE FROM convocacao where id = $1',
+        [id]
+    )
+    return {mensagem: "Deu boa feio!"}
+})
+
 servidor.listen({
     port: 3000
 })
